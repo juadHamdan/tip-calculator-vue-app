3dd467a76152d74aafaa9f723cce51c00e827955
Joad Rami Hamdan <64545813+juadHamdan@users.noreply.github.com>
2022-05-29 22:58:31 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <TipCalc />
+</template>
+
+<script>
+import TipCalc from "./components/TipCalc.vue";
+
+export default {
+  name: "App",
+  components: {
+    TipCalc,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Space Mono;
+  font-weight: 700;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
